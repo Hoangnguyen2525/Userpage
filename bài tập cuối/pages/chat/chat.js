30f4310ef44b3d892a1b50fd0e67ddc03709cd8f
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-04-12 15:45:33 +0700

    Add files via upload

---
+++ bài tập cuối/pages/chat/chat.js
@@ -0,0 +1,31 @@
+import { ConversationList } from "./conversationList.js";
+
+class Chat {
+  container = document.createElement("div");
+
+  conversationList = new ConversationList();
+
+  constructor() {
+    this.container.appendChild(this.conversationList.container);
+    this.container.setAttribute("class", "col-lg-2")
+    this.subscribeConversation()
+  }
+    subscribeConversation = () => {
+      db.collection("conversations").onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            // console.log("new conversation: ", change.doc.data());
+          }
+          if (change.type === "modified") {
+            console.log("Modified conversation: ", change.doc.data());
+          }
+          if (change.type === "removed") {
+            console.log("Removed conversation: ", change.doc.data());
+          }
+        });
+      });
+    }
+
+}
+
+export { Chat };
